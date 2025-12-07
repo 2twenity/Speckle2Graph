b2c9cde694cae06967c948f41b99270322cc26cd
Arseney <arseneyworking@gmail.com>
2025-12-07 16:22:45 +0100

    Simple neo4j query was added

--- src/speckle2graph/neo4j_queries/basic_query.py
+++ src/speckle2graph/neo4j_queries/basic_query.py
@@ -1,3 +1,62 @@
-def write_logical_graph():
-    pass
+from speckle2graph.graph_builders.simple_graph_builder import GraphBuilder
+from neo4j import GraphDatabase
+import tqdm 
 
+def write_logical_graph_to_neo4j(graph_builder_object: GraphBuilder, neo4j_client_driver):
+    for node in tqdm(graph_builder_object.logical_graph.nodes(data=True), desc="Writing Nodes to Neo4j"):
+        id_for_neo4j = node[0]
+        name_for_neo4j = node[1].get("name", "BIMELement")
+        speckle_type_for_neo4j = node[1].get("speckle_type", "Unknown")
+
+        neo4j_client_driver.execute_query("""CREATE (n {name: $name, id: $id, speckle_type: $speckle_type})""",
+                            name = name_for_neo4j,
+                            id = id_for_neo4j,
+                            speckle_type = speckle_type_for_neo4j
+                        )
+        
+    for edge in tqdm(graph_builder_object.logical_graph.edges(data=True), desc="Writing Edges to Neo4j"):
+        neo4j_client_driver.execute_query("""MATCH (n1 {id: $source_id}),(n2 {id: $target_id})
+                                        CREATE (n1)-[:CONTAINS {name: $connect_name}]->(n2)""",
+                                        source_id = edge[0],
+                                        target_id = edge[1],
+                                        connect_name = edge[2]["name"]
+                                    )
+        
+def write_geometrical_graph_to_neo4j(graph_builder_object: GraphBuilder, neo4j_client_driver):
+    for node in tqdm(graph_builder_object.geometrical_graph.nodes(data=True), desc="Writing Geometrical Nodes to Neo4j"):
+        # print(node)
+        name_for_neo4j = node[1]['name']
+        builtIn_category_for_neo4j = node[1]['builtIn_category']
+        revitId_for_neo4j = node[1]['revitId']
+        centroid_for_neo4j = node[1]['centroid']
+
+        neo4j_client_driver.execute_query("""UNWIND $batch AS row
+                                        MATCH (n1 {id: $node_id})
+                                        SET n1.name = $name_for_neo4j
+                                        SET n1.builtIn_category = $builtIn_category_for_neo4j
+                                        SET n1.revitId = $revitId_for_neo4j
+                                        SET n1.centroid = $centroid_for_neo4j
+                                        SET n1 += row
+                                        """,
+                                        node_id = node[0],
+                                        name_for_neo4j = name_for_neo4j,
+                                        builtIn_category_for_neo4j = builtIn_category_for_neo4j,
+                                        revitId_for_neo4j = revitId_for_neo4j,
+                                        centroid_for_neo4j = centroid_for_neo4j,
+                                        batch = node[1]['properties']
+                )
+        
+    for edge in tqdm(graph_builder_object.geometrical_graph.edges(data=True), desc="Writing Geometrical Edges to Neo4j"):
+        first_node_name = graph_builder_object.geometrical_graph.nodes[edge[0]]["name"]
+        second_node_name = graph_builder_object.geometrical_graph.nodes[edge[1]]["name"]
+
+        distance = edge[2]['distance']
+
+        neo4j_client_driver.execute_query("""MATCH (n1 {id: $source_id}),(n2 {id: $target_id})
+                               CREATE (n1)-[:CONNECTED_TO {name: $connect_name, distance: $distance}]->(n2)""",
+                            source_id = edge[0],
+                            target_id = edge[1],
+                            first_node_name = first_node_name,
+                            second_node_name = second_node_name,
+                            distance = distance,
+                            connect_name = edge[2]["name"])
